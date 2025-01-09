47907c3b6d39e302293543979f3e982398c6b4aa
hyosin <cdnnnl@gmail.com>
2025-01-09 22:17:30 +0900

    setup

---
+++ src/pages/ProfilePage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Button, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const ProfilePage = ({navigation}: any) => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ProfilePage</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          navigation.navigate('HomeScreens');
+        }}
+      />
+    </View>
+  );
+};
+
+export default ProfilePage;
---
+++ src/pages/ScannerSelectAlbumPage.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View, Button, Text} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+
+const ScannerSelectAlbumPage = ({navigation}: any) => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ScannerSelectAlbumPage</Text>
+      <Button
+        title="Go to Home"
+        onPress={() => {
+          navigation.navigate('HomeScreens');
+        }}
+      />
+    </View>
+  );
+};
+
+export default ScannerSelectAlbumPage;
