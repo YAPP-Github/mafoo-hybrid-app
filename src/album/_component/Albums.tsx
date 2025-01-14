eeeecebb5abb6102008e6c2cef6f61bcf7137894
hyosin <cdnnnl@gmail.com>
2025-01-14 21:08:34 +0900

    feat: Album 컴포넌트 구현 (더미 연결)

---
+++ src/album/_component/Albums.tsx
@@ -0,0 +1,32 @@
+import { StyleSheet, View } from "react-native"
+import { albumList } from "../../dummy"
+import DraggableAlbum from "./DraggableAlbum"
+
+const Albums = () => {
+  return (
+    <View style={styles.container}>
+      {albumList.map((album, index) => (
+        <DraggableAlbum
+          key={album.albumId + index}
+          index={index}
+          album={album}
+          //moveAlbum={moveAlbum}
+        />
+      ))}
+    </View>
+  )
+}
+
+export default Albums
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "space-between",
+    padding: 24,
+    gap: 16,
+  },
+})
