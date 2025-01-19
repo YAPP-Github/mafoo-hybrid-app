bec887cebf082ea1e8f76de899f190a04b9113f5
hyosin <cdnnnl@gmail.com>
2025-01-19 21:35:09 +0900

    feat: ColorIcon size prop 적용

--- src/common/ColorIcon.tsx
+++ src/common/ColorIcon.tsx
@@ -1,34 +1,42 @@
-import React from "react"
-import { View, StyleSheet } from "react-native"
+import { View, StyleSheet, StyleProp, ViewStyle } from "react-native"
 import { VariantProps } from "class-variance-authority"
 
-import { ICON_NAME } from "../constants"
-import { colorIconVariants } from "../styles/variants"
-import { cn } from "../utils"
+import { ICON_NAME } from "@/constants"
+import { colorIconVariants } from "@/styles/variants"
 import iconMap from "./iconMap"
+import { AlbumType } from "@/album/types"
 
 export const iconSize = {
   medium: 24,
   large: 36,
 } as const
 
-// export interface ColorIconProps extends VariantProps<typeof colorIconVariants> {
-//   size?: keyof typeof iconSize // 아이콘 크기
-//   iconColor?: string // 아이콘 배경색
-// }
+export interface ColorIconProps extends VariantProps<typeof colorIconVariants> {
+  size?: keyof typeof iconSize
+  iconColor?: AlbumType
+  style?: StyleProp<ViewStyle>
+}
 
 /**
  * @param size 아이콘의 크기 (기본값: "medium")
- * @param iconColor 아이콘의 배경색 (기본값: "red")
+ * @param iconColor 아이콘의 배경색 (기본값: "HEART")
  */
-const ColorIcon = ({ size = "medium", iconColor = "red", ...props }: any) => {
+const ColorIcon = ({
+  size = "medium",
+  iconColor,
+  ...props
+}: ColorIconProps) => {
   const SvgIcon = iconMap[ICON_NAME[iconColor || "HEART"]].default
 
   return (
     <View
       style={[styles.container, props.style]}
       className={`${colorIconVariants({ size, iconColor })}`}>
-      <SvgIcon width={24} height={24} color="white" />
+      <SvgIcon
+        width={iconSize[size || "medium"]}
+        height={iconSize[size || "medium"]}
+        color="white"
+      />
     </View>
   )
 }
