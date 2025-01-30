5545aa955e650e4a62f7eea774ea8ab44cccd7b4
hyosin <cdnnnl@gmail.com>
2025-01-30 16:03:43 +0900

    style: 새 앨범 만들기 boxShadow 적용

--- src/album/_component/NewAlbumButton.tsx
+++ src/album/_component/NewAlbumButton.tsx
@@ -1,8 +1,8 @@
 import { View, TouchableOpacity, StyleSheet } from "react-native"
 import { useNavigation } from "@react-navigation/native"
 import { StackNavigationProp } from "@react-navigation/stack"
-import MFText from "../../common/MFText"
-import NewAlbum from "../../assets/newAlbum.svg"
+import MFText from "@/common/MFText"
+import NewAlbum from "@/assets/newAlbum.svg"
 
 export type RootStackParamList = {
   AlbumCreate: undefined
@@ -21,10 +21,7 @@ const NewAlbumButton = () => {
       accessibilityLabel="새 앨범 만들기"
       style={styles.link}>
       <View style={styles.buttonContainer}>
-        <MFText
-          weight="SemiBold"
-          className="text-body1"
-          style={styles.buttonText}>
+        <MFText weight="SemiBold" className="text-body1 text-sumone-white">
           새 앨범 만들기
         </MFText>
         <NewAlbum size={20} color="white" />
@@ -41,6 +38,14 @@ const styles = StyleSheet.create({
     bottom: 114,
     left: "50%",
     transform: [{ translateX: -79 }],
+    // iOS shadow 설정
+    shadowColor: "rgba(101, 125, 159, 1)", // 그림자 색상
+    shadowOffset: { width: 0, height: 16 }, // X, Y 방향 그림자 위치
+    shadowOpacity: 0.12, // 그림자 투명도
+    shadowRadius: 20, // 그림자 흐림 정도
+    // android shadow 설정
+    elevation: 4,
+    zIndex: 10,
   },
   buttonContainer: {
     flexDirection: "row",
@@ -50,10 +55,5 @@ const styles = StyleSheet.create({
     paddingVertical: 15,
     paddingHorizontal: 22,
     borderRadius: 9999,
-  },
-  buttonText: {
-    color: "#FFFFFF",
-    fontSize: 16,
-    fontWeight: "500",
   },
 })
