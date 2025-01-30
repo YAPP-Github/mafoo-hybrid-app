4c4f912f92d31b2538a2692fd56ba2f6f85ecc5f
hyosin <cdnnnl@gmail.com>
2025-01-30 16:19:04 +0900

    feat: 앨범 페이지 PageContainer, FlatList 적용

--- src/album/_component/Albums.tsx
+++ src/album/_component/Albums.tsx
@@ -1,18 +1,29 @@
 import { StyleSheet, View } from "react-native"
-import { albumList } from "../../dummy"
+import { FlatList } from "react-native-gesture-handler"
+import { albumList } from "@/dummy"
 import DraggableAlbum from "./DraggableAlbum"
+// import { useGetAlbums } from "@/hooks/usePhoto"
 
 const Albums = () => {
+  //const { albums } = useGetAlbums()
+
   return (
     <View style={styles.container}>
-      {albumList.map((album, index) => (
-        <DraggableAlbum
-          key={album.albumId + index}
-          index={index}
-          album={album}
-          //moveAlbum={moveAlbum}
-        />
-      ))}
+      {/* FlatList: 화면에 보이는 데이터만 렌더링 */}
+      <FlatList
+        data={albumList}
+        keyExtractor={(item, index) => item.albumId + index}
+        renderItem={({ item, index }) => (
+          <DraggableAlbum
+            key={item.albumId + index}
+            index={index}
+            album={item}
+            // moveAlbum={moveAlbum}
+          />
+        )}
+        numColumns={2}
+        columnWrapperStyle={styles.row}
+      />
     </View>
   )
 }
@@ -23,9 +34,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
     padding: 24,
-    gap: 20,
+  },
+  row: {
+    justifyContent: "space-between",
+    marginBottom: 20,
   },
 })
