a005acc343c2f730ad0b1a125271e62f9cbddc6a
hyosin <cdnnnl@gmail.com>
2025-02-02 20:34:41 +0900

    test: 3초 뒤 Recap 페이지 이동

--- src/album/_component/VideoLoading.tsx
+++ src/album/_component/VideoLoading.tsx
@@ -27,12 +27,14 @@ const VideoLoading = ({
   const navigation = useNavigation<StackNavigationProp<RootStackParams>>()
 
   // TODO: 3초 뒤 Recap 페이지로 이동 (테스트 후 삭제)
-  // useEffect(() => {
-  //   setTimeout(() => {
-  //     navigation.navigate("Recap")
-  //     closeRecapModal()
-  //   }, 3000)
-  // }, [])
+  useEffect(() => {
+    if (visible) {
+      setTimeout(() => {
+        navigation.navigate("Recap")
+        closeRecapModal()
+      }, 3000)
+    }
+  }, [visible])
 
   return (
     <Modal visible={visible} transparent>
