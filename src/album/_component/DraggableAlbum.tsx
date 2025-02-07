1fd60e00c8555a407fc7caec536588d97bd6cad2
moonki <moonki0623@naver.com>
2025-02-08 01:35:57 +0900

    feat: 새 공유 앨범 있을 시 안내 ring 및 안내 text

--- src/album/_component/DraggableAlbum.tsx
+++ src/album/_component/DraggableAlbum.tsx
@@ -1,18 +1,24 @@
 import { useState } from "react"
-import { TouchableOpacity } from "react-native"
+import { Alert, Text, TouchableOpacity, View } from "react-native"
 import { useQueryClient } from "@tanstack/react-query"
 import { useNavigation } from "@react-navigation/native"
 
-import { deleteSharedMember, updateSharedMemberStatus } from "@/api/photo"
+import {
+  GetBulkAlbumResponse,
+  deleteSharedMember,
+  updateSharedMemberStatus,
+} from "@/api/photo"
 import { usePatchAlbumMove } from "../hooks/useAlbum"
 import AlbumItem from "./AlbumItem"
 import WaitingAlbumItem from "./WaitingAlbumItem"
 import { StackNavigationProp } from "@react-navigation/stack"
+import { ShareStatus } from "@/api/photo"
 
 interface AlbumItemProps {
-  album: any // GetBulkAlbumResponse
+  album: GetBulkAlbumResponse
   index: number
   moveAlbum?: (dragIndex: number, hoverIndex: number) => void // TODO: Required로 변경
+  showNewRing: boolean
 }
 const ItemType = "ALBUM"
 
@@ -20,7 +26,7 @@ export type RootStackParamList = {
   AlbumDetail: { albumId: string } | undefined
 }
 
-const DraggableAlbum = ({ album }: AlbumItemProps) => {
+const DraggableAlbum = ({ album, showNewRing }: AlbumItemProps) => {
   const queryClient = useQueryClient()
   const { patchAlbumMove } = usePatchAlbumMove()
   const isDisplayable = !album.shareStatus || album.shareStatus !== "REJECTED"
@@ -31,27 +37,40 @@ const DraggableAlbum = ({ album }: AlbumItemProps) => {
 
   const onTapAccept = () => {
     if (album.sharedMemberId) {
-      //   updateSharedMemberStatus(album.sharedMemberId, ShareStatus.ACCEPTED).then(
-      //     () => {
-      //       queryClient.invalidateQueries({ queryKey: ["getAlbums"] })
-      //     }
-      //   )
-      // }
+      updateSharedMemberStatus(album.sharedMemberId, ShareStatus.ACCEPTED).then(
+        () => {
+          queryClient.invalidateQueries({ queryKey: ["getAlbums"] })
+        }
+      )
     }
   }
 
   const onTapReject = () => {
-    if (album.sharedMemberId) {
-      deleteSharedMember(album.sharedMemberId).then(() => {
-        queryClient.invalidateQueries({ queryKey: ["getAlbums"] })
-      })
+    if (!album.sharedMemberId) {
+      return
     }
+
+    Alert.alert("앨범 공유 거절", "정말 앨범 공유를 거절할까요?", [
+      {
+        text: "취소",
+        style: "cancel",
+      },
+      {
+        text: "거절하기",
+        onPress: () => {
+          deleteSharedMember(album.sharedMemberId!).then(() => {
+            queryClient.invalidateQueries({ queryKey: ["getAlbums"] })
+          })
+        },
+        style: "destructive",
+      },
+    ])
   }
 
   const value = {
     ...album,
     photoCount: Number(album.photoCount),
-    isNew: false,
+    isNew: true,
     isSelected: album.albumId === isSelected,
     isEditable: false,
   }
@@ -69,6 +88,7 @@ const DraggableAlbum = ({ album }: AlbumItemProps) => {
         <AlbumItem value={value} handleValue={() => null} />
       ) : (
         <WaitingAlbumItem
+          showNewRing={showNewRing}
           value={value}
           onAccept={onTapAccept}
           onReject={onTapReject}
