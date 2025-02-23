c5fafb1aaf27d37672bc449db1a3d4bb0f6c42c3
hyosin <cdnnnl@gmail.com>
2025-02-23 16:56:22 +0900

    feat: fcm 토큰, 알림, 예약 api 추가

---
+++ src/api/notification.ts
@@ -0,0 +1,82 @@
+/** 알림 관련 API */
+
+import { NotificationsType } from "@/types/notifications"
+import { createFetcher } from "./myfetch"
+
+export interface NotificationUrl {
+  thumbnailImageUrl: string
+  url: string
+}
+
+// 알림함 수신
+type LocalNotification = NotificationUrl &
+  Notification & {
+    notificationType: NotificationsType
+  }
+
+export interface Notification {
+  notificationId: string
+  templateId: string
+  receiverMemberId: string
+  title: string
+  body: string
+  isRead: boolean
+  createdAt: string
+  updatedAt: string
+}
+
+export interface SenarioNotification {
+  notificationType: NotificationsType
+  receiverMemberIds: string[]
+  variables: unknown
+}
+
+export interface NotificationIds {
+  notificationIds: string[]
+}
+
+const authorizedFetcher = createFetcher("user/v1")
+
+/** 알림 목록 조회 */
+export const getNotificationList = async (
+  memberId: string | undefined
+): Promise<LocalNotification[]> => {
+  const data = await authorizedFetcher.get(
+    `/notifications?memberId=${memberId}`
+  )
+  return data
+}
+
+/** 시나리오 알림 전송 (NOTE: 테스트용, 추후 삭제) */
+export const sendSenarioNotification = async (
+  body: SenarioNotification
+): Promise<Notification[]> => {
+  const data = await authorizedFetcher.post(`/notifications`, body)
+  return data
+}
+
+/** 알림 n건 삭제 */
+export const deleteBulkNotifications = async (
+  memberId: string | undefined,
+  body: NotificationIds
+): Promise<NotificationIds> => {
+  const data = await authorizedFetcher.delete(
+    `/notifications?memberId=${memberId}`,
+    { data: body }
+  )
+  return data
+}
+
+/** 알림 읽음 여부 n건 수정 */
+export const markNotificationsAsRead = async (
+  memberId: string,
+  notificationIds: string[]
+): Promise<Notification[]> => {
+  const data = await authorizedFetcher.patch(
+    `/notifications?memberId=${memberId}`,
+    {
+      notificationIds: notificationIds,
+    }
+  )
+  return data
+}
