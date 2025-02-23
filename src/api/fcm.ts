297e8b11d5a5172109c3fbd23030c5664c249daf
hyosin <cdnnnl@gmail.com>
2025-02-23 16:56:22 +0900

    feat: fcm 토큰, 알림, 예약 api 추가

---
+++ src/api/fcm.ts
@@ -0,0 +1,54 @@
+/** FCM 토큰 관련 API */
+
+import { createFetcher } from "./myfetch"
+
+export interface FcmToken {
+  fcmTokenId: string
+  ownerMemberId: string
+  token: string
+  createdAt: string
+  updatedAt: string
+}
+
+const authorizedFetcher = createFetcher("user/v1")
+
+/** 토큰 목록 조회 */
+export const getFcmTokenList = async (
+  memberId: string | undefined
+): Promise<FcmToken[]> => {
+  const data = await authorizedFetcher.get(`/fcm-tokens?memberId=${memberId}`)
+  return data
+}
+
+/** 토큰 수정 */
+export const updateFcmToken = async (
+  memberId: string,
+  fcmToken: string
+): Promise<FcmToken> => {
+  const data = await authorizedFetcher.put(`/fcm-tokens?memberId=${memberId}`, {
+    token: fcmToken,
+  })
+  return data
+}
+
+/** 토큰 생성 */
+export const createFcmToken = async (
+  memberId: string,
+  fcmToken: string
+): Promise<FcmToken> => {
+  const data = await authorizedFetcher.post(
+    `/fcm-tokens?memberId=${memberId}`,
+    {
+      token: fcmToken,
+    }
+  )
+  return data
+}
+
+/** 토큰 삭제 */
+export const deleteFcmToken = async (memberId: string | undefined) => {
+  const data = await authorizedFetcher.delete(
+    `/fcm-tokens?memberId=${memberId}`
+  )
+  return data
+}
