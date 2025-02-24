0f34eda79a08437e32bbf4ee4881cb7990e8ac04
hyosin <cdnnnl@gmail.com>
2025-02-24 23:15:02 +0900

    feat: 알림 조회, 읽음 처리, 삭제 query 추가

---
+++ src/constants/queryString.ts
@@ -0,0 +1,7 @@
+/** tanstack-query queryKey */
+
+export const NOTIFICATIONS = {
+  GET_NOTIFICATIONS: ["getNotifications"],
+  READ_NOTIFICATIONS: ["readNotification"],
+  DELETE_NOTIFICATIONS: ["deleteNotifications"],
+}
