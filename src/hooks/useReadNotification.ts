5a7d6d468b5814c0853b900337f47b1b76367681
hyosin <cdnnnl@gmail.com>
2025-03-01 17:56:34 +0900

    feat: fcm backgroundEvent 처리

--- src/hooks/useReadNotification.ts
+++ src/hooks/useReadNotification.ts
@@ -1,8 +1,5 @@
 import { markNotificationsAsRead } from "@/api/notification"
-import { useNavigation } from "@react-navigation/native"
 import { useMutation, useQueryClient } from "@tanstack/react-query"
-import { getRouteParams } from "./useForegroundEvent"
-import { Notification } from "@notifee/react-native"
 import { NotificationProps } from "@/album/_component/notification"
 import { NOTIFICATIONS } from "@/constants/queryString"
 
@@ -13,47 +10,42 @@ import { NOTIFICATIONS } from "@/constants/queryString"
 */
 export const useReadNotification = (
   memberId: string,
-  notificationIds: string[],
-  move: boolean, // 이동 여부
-  params: any
+  notificationIds: string[]
 ) => {
   const queryClient = useQueryClient()
-  const navigation = useNavigation()
+
+  console.log("useread notificationIds", notificationIds)
 
   const { mutate } = useMutation({
     mutationKey: [...NOTIFICATIONS.READ_NOTIFICATIONS],
     mutationFn: (notificationIds: string[]) =>
       markNotificationsAsRead(memberId, notificationIds),
-    onSuccess: (data) => {
-      /** 낙관적 업데이트 */
-
+    onSuccess: () => {
       queryClient.setQueryData(
         [...NOTIFICATIONS.GET_NOTIFICATIONS],
+
         (prev: NotificationProps[]) => {
-          // console.log("oldData", prev, notificationIds)
           if (!prev) return prev
+
           const newData = JSON.parse(JSON.stringify(prev))
+
           notificationIds?.map((notificationId) => {
             const readNotificationIdx = prev.findIndex(
               (noti) => noti.notificationId === notificationId
             )
+
             if (readNotificationIdx !== -1) {
               newData[readNotificationIdx].isRead = true
             }
           })
-          // console.log("newData", newData)
+
           return newData
         }
       )
-      // params 에 따라 페이지 이동 (알림함, 시스템 공통)
-      // if (move) {
-      //   navigation.navigate(
-      //     (params?.routes as any) ?? "AlbumCreate",
-      //     getRouteParams(params as any)
-      //   )
-      // }
     },
-    onError: () => {},
+    onError: (e) => {
+      console.error("알림 읽음", e)
+    },
   })
   return { mutate }
 }
