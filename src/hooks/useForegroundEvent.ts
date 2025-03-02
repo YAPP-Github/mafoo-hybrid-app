f4b181e430e33f56608acd7d95aad80a669b4aae
hyosin <cdnnnl@gmail.com>
2025-03-02 10:13:29 +0900

    feat: paramKey로 변경

--- src/hooks/useForegroundEvent.ts
+++ src/hooks/useForegroundEvent.ts
@@ -7,6 +7,8 @@ import { fcmNotificationParams } from "@/types/notifications"
 import { fcmNotification } from "./useForegroundMessage"
 import { useAuth } from "@/store/auth/AuthProvider"
 import { RootStackParamList } from "@/types/routeParams"
+import { getMyProfile } from "@/api/signIn"
+import { markNotificationsAsRead } from "@/api/notification"
 
 /**
  * foreground 푸시 알림 이벤트 수신 핸들러
@@ -25,7 +27,7 @@ export function getRouteParams(
   data:
     | {
         route: keyof RootStackParamList
-        key: string | null
+        paramKey: string | null
         buttonType?: null | boolean
       }
     | undefined
@@ -38,12 +40,12 @@ export function getRouteParams(
   // `route`에 따라 `key` 값을 다르게 설정
   switch (data.route) {
     case "Recap":
-      result = { recapUrl: data.key }
+      result = { recapUrl: data.paramKey }
       break
     case "SharedFriend":
     case "AlbumDetail":
     case "AddFriend":
-      result = { albumId: data.key }
+      result = { albumId: data.paramKey }
       break
     default:
       result = {} // 다른 route인 경우 기본 값
@@ -58,16 +60,35 @@ export function useForegroundEvent() {
 
   const { status } = useAuth()
 
-  // const isSignedIn = status === "signIn"
+  const isSignedIn = status === "signIn"
 
-  // const { profile } = isSignedIn ? useGetProfile() : { profile: null }
-  // const { mutate } = isSignedIn
-  //   ? useReadNotification(profile?.memberId ?? "", [])
-  //   : { mutate: null }
+  const readAndMove = async (data: any) => {
+    try {
+      const profile = await getMyProfile()
+
+      if (isSignedIn && profile?.memberId && data?.notificationId) {
+        await markNotificationsAsRead(profile.memberId, [data.notificationId])
+
+        navigation.navigate(
+          (data?.route as any) ?? "AlbumCreate",
+          getRouteParams(data as any)
+        )
+      } else {
+        console.log(
+          "이동 못함 isSignedIn, memberId, notificationId",
+          isSignedIn,
+          profile?.memberId,
+          data?.notificationId
+        )
+      }
+    } catch (e) {
+      console.error("me", e)
+      // 이미 삭제된 알림인 경우 Toast 메시지 추가
+    }
+  }
 
   useEffect(() => {
     // iOS, android
-
     const unsubscribe = notifee.onForegroundEvent(async ({ type, detail }) => {
       console.log("foreground event!!")
       if (
@@ -76,15 +97,11 @@ export function useForegroundEvent() {
           detail.notification?.ios?.categoryId === "open_detail_category") // ios
       ) {
         const data = detail?.notification?.data
-
-        // if (isSignedIn && profile?.memberId && data?.notificationId && mutate) {
-        //   // mutate([data.notificationId] as any)
-
-        //   navigation.navigate(
-        //     (data?.route as any) ?? "AlbumCreate",
-        //     getRouteParams(data as any)
-        //   )
-        // }
+        if (data) {
+          readAndMove(data)
+        } else {
+          console.error("data 없음")
+        }
       }
     })
 
