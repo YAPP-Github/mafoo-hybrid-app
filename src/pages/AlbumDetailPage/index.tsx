f38f69fd83c5f12457222cef44090019be43cd93
hyosin <cdnnnl@gmail.com>
2025-03-16 15:28:40 +0900

    fix: ShareBar 누락된 key prop 추가

--- src/pages/AlbumDetailPage/index.tsx
+++ src/pages/AlbumDetailPage/index.tsx
@@ -256,6 +256,7 @@ const ShareBar: React.FC<ShareBarProps> = ({
           <View className="flex-row h-11">
             {previewMembers.slice(0, 4)?.map((member, idx) => (
               <View
+                key={`sharebar-${member.memberId}`}
                 className="relative"
                 style={{
                   zIndex: 10 + (5 - idx),
